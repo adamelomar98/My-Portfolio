e653a46f813d64e51eff96ee4db3a45aa517ddec
adamelomar98 <adam.elomar.98@gmail.com>
2023-10-02 18:59:20 +0300

    fixing footer color

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border-t z-10 border-t-[#33353F]  bg-gradient-to-b from-[#121212] to-black w-full flex items-center justify-center ">
+    <footer className="footer border-t z-10 border-t-[#33353F]  bg-[#121212] w-full flex items-center justify-center ">
       <div className="container p-12  flex justify-between ">
         <span>ADAM.</span>
         <p className="text-slate-600">All rights reserved.</p>
